e7075301c1731a9448a14aeb57e7d9fde1ccb4d7
felpzw <lipewinsch@gmail.com>
2025-11-09 13:12:17 -0300

    Block.data generic type

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,19 @@
 pub mod pocket_chain {
 
     pub mod blockchain{
+        use std::fmt::Debug;
+
         use serde::Serialize;
+        use serde_json::json;
         use sha2::{Digest, Sha256};
+        use chrono::Utc;
 
 
         #[derive(Debug, Clone, Serialize)]
-        pub struct Block {
+        pub struct Block<T> {
             pub index: u64,
             pub timestamp: String,
-            pub data: String,
+            pub data: T,
             pub previous_hash: String,
             pub nonce: u64,
 
@@ -17,7 +21,7 @@ pub mod pocket_chain {
             pub hash: String,
         }
 
-        impl Block {
+        impl<T: Serialize> Block<T> {
             pub fn calculate_hash(&self) -> String {
 
                 let data_as_json = serde_json::to_string(self).expect("Serialization error");
@@ -30,7 +34,29 @@ pub mod pocket_chain {
 
             }  
 
-        } 
+        }
+        
+        
+        pub struct Blockchain<T> {
+            pub chain: Vec<Block<T>>
+        }
+
+        impl<T: Serialize + Default + Debug> Blockchain<T> {
+            pub fn new(difficult: u32) -> Self
+            {   
+                let mut genesis = Block {
+                    index: 0,
+                    timestamp: Utc::now().to_string(),
+                    data: T::default(),
+                    previous_hash: String::from("0"),
+                    nonce: 0,
+                    hash: String::new(),
+                 };
+                genesis.hash = genesis.calculate_hash();
+                println!("{:?}", genesis);
+                Blockchain { chain: vec![genesis] }
+            }
+        }
 
 
     }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use pocket_blockchain::pocket_chain::blockchain::{Blockchain};
 fn main() {
     println!("Hello, world!");
+    let bc = Blockchain::<String>::new(5);
+    print!("{:?}", bc.chain[0])
 }
